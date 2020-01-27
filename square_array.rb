397a9732478479b93a9f28c919e4aa048c67429d
Claudia Pierson <c172@umbc.edu>
2020-01-27 03:46:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
-def square_array(array)
-  result= []
-  array.each do |jath|
-   result.push(jath **2 )
-  end
-  return result
-end
+#def square_array(array)
+  #result= []
+  #array.each do |jath|
+   #result.push(jath **2 )
+  #end
+  #return result
+#end
